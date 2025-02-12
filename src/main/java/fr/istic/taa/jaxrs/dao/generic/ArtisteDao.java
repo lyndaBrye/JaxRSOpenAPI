8718cb9efa672bf8057a15ab915308ea03188d2a
lyndaBrye <lyndabrye01@gmail.com>
2025-02-12 08:26:04 +0100

    ArtisteDAO update

--- src/main/java/fr/istic/taa/jaxrs/dao/generic/ArtisteDao.java
+++ src/main/java/fr/istic/taa/jaxrs/dao/generic/ArtisteDao.java
@@ -11,16 +11,19 @@ public class ArtisteDao extends AbstractJpaDao<Long, Artiste> {
         setClazz(Artiste.class);
     }
 
-
-    public List<Artiste> findAllOrderedByNom() {
-        TypedQuery<Artiste> query = entityManager.createQuery("SELECT a FROM Artiste a ORDER BY a.nom", Artiste.class);
-        return query.getResultList();
-    }
-
     public List<Artiste> findByNom(String nom) {
         TypedQuery<Artiste> query = entityManager.createQuery("SELECT a FROM Artiste a WHERE a.nom = :nom", Artiste.class);
         query.setParameter("nom", nom);
         return query.getResultList();
     }
 
+    public List<Artiste> findAllOrderedByNom() {
+        TypedQuery<Artiste> query = entityManager.createQuery("SELECT a FROM Artiste a ORDER BY a.nom", Artiste.class);
+        return query.getResultList();
+    }
+
+    public long count() {
+        return entityManager.createQuery("SELECT COUNT(a) FROM Artiste a", Long.class)
+                .getSingleResult();
+    }
 }
